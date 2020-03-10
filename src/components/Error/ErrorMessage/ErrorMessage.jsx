16b31e4faedde58f616844f62e2ef49b9a27e226
Lenar <lenar.evstafyev@mail.ru>
2020-03-10 23:35:12 +0300

    added ErrorBoundery

---
+++ src/components/Error/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './error-message.css';
+
+const ErrorMessage = () => {
+    return <div class='еrror-wrap'><h2>Произошла ошибка</h2>
+    <h3>Проблема уже решается, повторите попытку позднее</h3></div>;
+};
+
+export default ErrorMessage;
